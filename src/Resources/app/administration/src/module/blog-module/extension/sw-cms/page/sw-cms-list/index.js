ad9f8e82bff1b9c16bb81dd0bbda81d38ccba1bb
Daniel <revoltek-daniel@users.noreply.github.com>
2023-05-11 15:25:27 +0200

    use new cmspagetypeservice

--- src/Resources/app/administration/src/module/blog-module/extension/sw-cms/page/sw-cms-list/index.js
+++ src/Resources/app/administration/src/module/blog-module/extension/sw-cms/page/sw-cms-list/index.js
@@ -1,16 +1,11 @@
 const { Component } = Shopware;
 
 Component.override('sw-cms-list', {
-    computed: {
-        sortPageTypes() {
-            return [
-                { value: '', name: this.$tc('sw-cms.sorting.labelSortByAllPages'), active: true },
-                { value: 'page', name: this.$tc('sw-cms.sorting.labelSortByShopPages') },
-                { value: 'landingpage', name: this.$tc('sw-cms.sorting.labelSortByLandingPages') },
-                { value: 'product_list', name: this.$tc('sw-cms.sorting.labelSortByCategoryPages') },
-                { value: 'product_detail', name: this.$tc('sw-cms.sorting.labelSortByProductPages') },
-                { value: 'blog_detail', name: this.$tc('sw-cms.sorting.labelSortByBlogPages') },
-            ];
-        },
+    methods: {
+        createdComponent() {
+            this.cmsPageTypeService.register({name: 'blog_detail', icon: 'egular-tag', title: 'sw-cms.sorting.labelSortByBlogPages'})
+
+            this.$super('createdComponent');
+        }
     },
 });
